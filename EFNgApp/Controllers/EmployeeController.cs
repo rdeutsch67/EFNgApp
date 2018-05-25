e37bde353c6ac2df2a4f2c4cd158c2b1563224c6
rdeutsch67 <robert.deutschmann@ekor.ch>
2018-05-25 11:52:31 +0200

    Projektdateien hinzufügen.

---
+++ EFNgApp/Controllers/EmployeeController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using EFNgApp.Models;
+using Microsoft.AspNetCore.Mvc;
+namespace EFNgApp.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        EmployeeDataAccessLayer objemployee = new EmployeeDataAccessLayer();
+
+        [HttpGet]
+        [Route("api/Employee/Index")]
+        public IEnumerable<TblEmployee> Index()
+        {
+            return objemployee.GetAllEmployees();
+        }
+
+        [HttpPost]
+        [Route("api/Employee/Create")]
+        public int Create([FromBody] TblEmployee employee)
+        {
+            return objemployee.AddEmployee(employee);
+        }
+
+        [HttpGet]
+        [Route("api/Employee/Details/{id}")]
+        public TblEmployee Details(int id)
+        {
+            return objemployee.GetEmployeeData(id);
+        }
+
+        [HttpPut]
+        [Route("api/Employee/Edit")]
+        public int Edit([FromBody]TblEmployee employee)
+        {
+            return objemployee.UpdateEmployee(employee);
+        }
+
+        [HttpDelete]
+        [Route("api/Employee/Delete/{id}")]
+        public int Delete(int id)
+        {
+            return objemployee.DeleteEmployee(id);
+        }
+
+        [HttpGet]
+        [Route("api/Employee/GetCityList")]
+        public IEnumerable<TblCities> Details()
+        {
+            return objemployee.GetCities();
+        }
+    }
+}
